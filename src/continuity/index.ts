2b191968896a48a92f0653b86ea4ee1b94d2b0eb
Maxime Houle <maximehoule100@gmail.com>
2026-08-13 21:58:49 -0400

    feat(continuity): add harness-neutral resume kernel

---
+++ src/continuity/index.ts
@@ -0,0 +1,7 @@
+export * from "./adapter.js";
+export * from "./fold.js";
+export * from "./fs-ledger.js";
+export * from "./markdown.js";
+export * from "./normalize.js";
+export * from "./projection.js";
+export * from "./types.js";
